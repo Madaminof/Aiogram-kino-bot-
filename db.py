e899742cf6cf51b087176cb5d3cad3a97abb9bf3
Madaminof <madaminovsamandar51@gmail.com>
2024-12-10 10:19:57 +0500

    csddc

--- db.py
+++ db.py
@@ -1,19 +1,25 @@
 import os
 import json
 
+
 def get_message_id(kino_kodi):
     base_dir = os.path.dirname(__file__)
     json_path = os.path.join(base_dir, "kino_codes.json")
 
     try:
-        # Faylni o'qishdan oldin, yangilanishini majburlash
+        # Faylni ochish
         with open(json_path, "r", encoding="utf-8") as file:
-            os.fsync(file.fileno())  # Faylni tizim keshidan yangilash
             data = json.load(file)
-            return data.get(kino_kodi)
+
+        # Kino kodi bo'yicha message_id ni qaytarish
+        return data.get(kino_kodi)
+
     except FileNotFoundError:
         print(f"Fayl topilmadi: {json_path}")
         return None
     except json.JSONDecodeError:
         print("JSON formatida xatolik mavjud.")
+        return None
+    except Exception as e:
+        print(f"Kutilmagan xatolik: {e}")
         return None
